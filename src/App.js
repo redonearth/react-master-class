31b47899b613013441a4e40de678c6664bd23afc
Jisang Hong <wona23@gmail.com>
2021-11-16 14:52:46 +0900

    Animation, selector 적용 학습

--- src/App.js
+++ src/App.js
@@ -1,22 +1,55 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
-const Father = styled.div`
+const Wrapper = styled.div`
   display: flex;
+  height: 100vh;
+  width: 100vw;
+  justify-content: center;
+  align-items: center;
 `;
 
-const Input = styled.input.attrs({ required: true })`
+const rotateAnimation = keyframes`
+  0% {
+    transform: rotate(0deg);
+    border-radius: 0px;
+  }
+  50% {
+    border-radius: 100px;
+  }
+  100% {
+    transform: rotate(360deg);
+    border-radius: 0px;
+  }
+`;
+
+const Box = styled.div`
+  height: 100px;
+  width: 100px;
   background-color: tomato;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  animation: ${rotateAnimation} 1s linear infinite;
+
+  span {
+    font-size: 42px;
+
+    &:hover {
+      font-size: 56px;
+    }
+    &:active {
+      opacity: 0;
+    }
+  }
 `;
 
 function App() {
   return (
-    <Father as="header">
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-    </Father>
+    <Wrapper>
+      <Box>
+        <span>🥰</span>
+      </Box>
+    </Wrapper>
   );
 }
 
