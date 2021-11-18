c5b24164a0b93a146033ddb9a225a30b68080026
Jisang Hong <wona23@gmail.com>
2021-11-18 15:29:32 +0900

    타입스크립트 theme 적용 학습

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,33 @@
-import React, { useState } from 'react';
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
+
+interface DummyProps {
+  text: string;
+  active?: boolean;
+}
+
+function Dummy({ text, active = false }: DummyProps) {
+  return <H1>{text}</H1>;
+}
 
 function App() {
-  const [username, setUsername] = useState('');
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setUsername(value);
-  };
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
-    console.log('Hello,', username);
+    console.log(event.currentTarget);
   };
+
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input
-          value={username}
-          onChange={onChange}
-          type="text"
-          placeholder="username"
-        />
-        <button>Login</button>
-      </form>
-    </div>
+    <Container>
+      <Dummy text="hello" active />
+      <button onClick={onClick}>Click me</button>
+    </Container>
   );
 }
 
