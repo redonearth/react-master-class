f93f9d4a9fa65f65813ea59f33486b860ab7507f
Jisang Hong <wona23@gmail.com>
2021-11-18 16:52:26 +0900

    Router 환경 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,7 @@
-import React from 'react';
-import styled from 'styled-components';
-
-const Container = styled.div`
-  background-color: ${(props) => props.theme.bgColor};
-`;
-const H1 = styled.h1`
-  color: ${(props) => props.theme.textColor};
-`;
-
-interface DummyProps {
-  text: string;
-  active?: boolean;
-}
-
-function Dummy({ text, active = false }: DummyProps) {
-  return <H1>{text}</H1>;
-}
+import Router from './Router';
 
 function App() {
-  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    event.preventDefault();
-    console.log(event.currentTarget);
-  };
-
-  return (
-    <Container>
-      <Dummy text="hello" active />
-      <button onClick={onClick}>Click me</button>
-    </Container>
-  );
+  return <Router />;
 }
 
 export default App;
