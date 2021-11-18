45839b922811e57356a75c2d710aa06b17e0c7a7
Jisang Hong <wona23@gmail.com>
2021-11-18 09:49:34 +0900

    타입스크립트 form 작성 학습

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
-import Circle from './Circle';
+import React, { useState } from 'react';
 
 function App() {
+  const [username, setUsername] = useState('');
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setUsername(value);
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log('Hello,', username);
+  };
   return (
     <div>
-      <Circle bgColor="teal" borderColor="yellow" />
-      <Circle bgColor="tomato" text="Second Circle" />
+      <form onSubmit={onSubmit}>
+        <input
+          value={username}
+          onChange={onChange}
+          type="text"
+          placeholder="username"
+        />
+        <button>Login</button>
+      </form>
     </div>
   );
 }
