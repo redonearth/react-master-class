7987eddbcb11002ffb4098d7d39ab36db708c638
Jisang Hong <wona23@gmail.com>
2021-11-19 11:14:32 +0900

    Home 페이지 코인 링크 리스트 및 스타일링 추가

--- src/theme.ts
+++ src/theme.ts
@@ -3,5 +3,5 @@ import { DefaultTheme } from 'styled-components';
 export const theme: DefaultTheme = {
   bgColor: '#1e3799',
   textColor: '#f5f6fa',
-  accentColor: '#26de81',
+  accentColor: '#4bcffa',
 };
