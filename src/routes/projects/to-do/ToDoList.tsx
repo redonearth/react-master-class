ff58e475050c1f2782de7b7ff0181dda88a92bd3
Jisang Hong <wona23@gmail.com>
2021-11-26 17:23:47 +0900

    Form Validation 추가

--- src/routes/projects/to-do/ToDoList.tsx
+++ src/routes/projects/to-do/ToDoList.tsx
@@ -29,17 +29,44 @@ import { useForm } from 'react-hook-form';
 // }
 
 function ToDoList() {
-  const { register, watch } = useForm();
-  console.log(watch());
+  const { register, handleSubmit, formState } = useForm();
+  const onValid = (data: any) => {
+    console.log(data);
+  };
+  console.log(formState.errors);
   return (
     <div>
-      <form style={{ display: 'flex', flexDirection: 'column' }}>
-        <input {...register('email')} placeholder="이메일" />
-        <input {...register('lastName')} placeholder="성" />
-        <input {...register('firstName')} placeholder="이름" />
-        <input {...register('username')} placeholder="표시할 이름" />
-        <input {...register('password')} placeholder="패스워드" />
-        <input {...register('passwordConfirm')} placeholder="패스워드 확인" />
+      <form
+        onSubmit={handleSubmit(onValid)}
+        style={{ display: 'flex', flexDirection: 'column' }}
+      >
+        <input
+          {...register('email', { required: '이메일은 필수입니다.' })}
+          placeholder="이메일"
+        />
+        <input {...register('lastName', { required: true })} placeholder="성" />
+        <input
+          {...register('firstName', { required: true })}
+          placeholder="이름"
+        />
+        <input
+          {...register('username', {
+            required: true,
+            minLength: {
+              value: 3,
+              message: '길이가 짧습니다. 3글자 이상 입력해주세요.',
+            },
+          })}
+          placeholder="표시할 이름"
+        />
+        <input
+          {...register('password', { required: true, minLength: 8 })}
+          placeholder="패스워드"
+        />
+        <input
+          {...register('passwordConfirm', { required: true, minLength: 8 })}
+          placeholder="패스워드 확인"
+        />
         <button>Add</button>
       </form>
     </div>
