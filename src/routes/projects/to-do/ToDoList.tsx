6ba7a98056e122295817cceb6234b65ac4bc2b00
Jisang Hong <wona23@gmail.com>
2021-11-29 10:00:36 +0900

    Form errors 추가

--- src/routes/projects/to-do/ToDoList.tsx
+++ src/routes/projects/to-do/ToDoList.tsx
@@ -28,12 +28,29 @@ import { useForm } from 'react-hook-form';
 //   );
 // }
 
+interface IForm {
+  email: string;
+  lastName: string;
+  firstName: string;
+  username: string;
+  password: string;
+  passwordConfirm: string;
+}
+
 function ToDoList() {
-  const { register, handleSubmit, formState } = useForm();
-  const onValid = (data: any) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IForm>({
+    defaultValues: {
+      email: '@gmail.com',
+    },
+  });
+  const onValid = (data: IForm) => {
     console.log(data);
   };
-  console.log(formState.errors);
+  console.log(errors);
   return (
     <div>
       <form
@@ -41,32 +58,53 @@ function ToDoList() {
         style={{ display: 'flex', flexDirection: 'column' }}
       >
         <input
-          {...register('email', { required: '이메일은 필수입니다.' })}
+          {...register('email', {
+            required: '이메일은 필수입니다.',
+            pattern: {
+              value: /^[A-Za-z0-9._%+-]+@gmail.com$/,
+              message: 'Gmail 계정만 허용됩니다.',
+            },
+          })}
           placeholder="이메일"
         />
-        <input {...register('lastName', { required: true })} placeholder="성" />
+        <span>{errors?.email?.message}</span>
         <input
-          {...register('firstName', { required: true })}
+          {...register('lastName', { required: '성을 입력하세요.' })}
+          placeholder="성"
+        />
+        <span>{errors?.lastName?.message}</span>
+        <input
+          {...register('firstName', { required: '이름을 입력하세요.' })}
           placeholder="이름"
         />
+        <span>{errors?.firstName?.message}</span>
         <input
           {...register('username', {
-            required: true,
+            required: '사용자명은 필수입니다.',
             minLength: {
               value: 3,
               message: '길이가 짧습니다. 3글자 이상 입력해주세요.',
             },
           })}
-          placeholder="표시할 이름"
+          placeholder="사용자명"
         />
+        <span>{errors?.username?.message}</span>
         <input
-          {...register('password', { required: true, minLength: 8 })}
+          {...register('password', {
+            required: '패스워드를 입력하세요.',
+            minLength: 8,
+          })}
           placeholder="패스워드"
         />
+        <span>{errors?.password?.message}</span>
         <input
-          {...register('passwordConfirm', { required: true, minLength: 8 })}
+          {...register('passwordConfirm', {
+            required: '패스워드를 한 번 더 입력하세요.',
+            minLength: 8,
+          })}
           placeholder="패스워드 확인"
         />
+        <span>{errors?.passwordConfirm?.message}</span>
         <button>Add</button>
       </form>
     </div>
