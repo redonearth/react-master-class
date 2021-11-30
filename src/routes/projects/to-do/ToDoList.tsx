aae7e33ecd87aa7cb32303ed59522fbc056f6a98
Jisang Hong <wona23@gmail.com>
2021-11-30 15:23:35 +0900

    상태에 따른 카테고리 분류 기능 추가

--- src/routes/projects/to-do/ToDoList.tsx
+++ src/routes/projects/to-do/ToDoList.tsx
@@ -1,21 +1,28 @@
-import { useRecoilValue } from 'recoil';
-import { toDoState } from './atoms';
+import React from 'react';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { categoryState, toDoSelector } from './atoms';
 import CreateToDo from './CreateToDo';
 import ToDo from './ToDo';
 
 function ToDoList() {
-  const toDos = useRecoilValue(toDoState);
-  console.log(toDos);
+  const toDos = useRecoilValue(toDoSelector);
+  const [category, setCategory] = useRecoilState(categoryState);
+  const onInput = (event: React.FormEvent<HTMLSelectElement>) => {
+    setCategory(event.currentTarget.value);
+  };
   return (
     <div>
       <h1>할 일</h1>
       <hr />
+      <select value={category} onInput={onInput}>
+        <option value="TO_DO">해야 함</option>
+        <option value="DOING">하는 중</option>
+        <option value="DONE">완료</option>
+      </select>
       <CreateToDo />
-      <ul>
-        {toDos.map((toDo) => (
-          <ToDo key={toDo.id} {...toDo} />
-        ))}
-      </ul>
+      {toDos?.map((toDo) => (
+        <ToDo key={toDo.id} {...toDo} />
+      ))}
     </div>
   );
 }
