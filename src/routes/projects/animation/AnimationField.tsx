76d6d4713d76548f1cb31ecba6e5a3ae92a3accb
Jisang Hong <wona23@gmail.com>
2021-12-24 16:38:28 +0900

    Children 요소 속성 적용

--- src/routes/projects/animation/AnimationField.tsx
+++ src/routes/projects/animation/AnimationField.tsx
@@ -1,4 +1,4 @@
-import { motion } from 'framer-motion';
+import { motion, Variants } from 'framer-motion';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
@@ -10,30 +10,63 @@ const Wrapper = styled.div`
 `;
 
 const Box = styled(motion.div)`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
   width: 200px;
   height: 200px;
-  background-color: white;
-  border-radius: 10px;
+  border-radius: 50px;
+  padding: 10px;
+  background-color: rgba(255, 255, 255, 0.2);
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const myVars = {
-  start: { scale: 0 },
+const Circle = styled(motion.div)`
+  place-self: center;
+  background-color: white;
+  width: 70px;
+  height: 70px;
+  border-radius: 50%;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const boxVariants: Variants = {
+  start: {
+    opacity: 0,
+    scale: 0.5,
+  },
   end: {
+    opacity: 1,
     scale: 1,
-    rotateZ: 360,
     transition: {
       type: 'spring',
-      bounce: 0.4,
-      delay: 0.5,
+      duration: 0.5,
+      bounce: 0.5,
+      delayChildren: 0.5,
+      staggerChildren: 0.1,
     },
+  },
+};
+
+const circleVarients: Variants = {
+  start: {
+    opacity: 0,
+    y: 10,
+  },
+  end: {
+    opacity: 1,
+    y: 0,
   },
 };
 
 function AnimationField() {
   return (
     <Wrapper>
-      <Box variants={myVars} initial="start" animate="end" />
+      <Box variants={boxVariants} initial="start" animate="end">
+        <Circle variants={circleVarients} />
+        <Circle variants={circleVarients} />
+        <Circle variants={circleVarients} />
+        <Circle variants={circleVarients} />
+      </Box>
     </Wrapper>
   );
 }
